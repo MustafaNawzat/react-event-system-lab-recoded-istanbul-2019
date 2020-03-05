81b87ad1376653e90027d7aba6862275b8bfc38f
Mustafa Nawzat <mustafanawzatt@gmail.com>
2020-03-05 07:07:47 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+
+export default class E extends Component{
+
+    enteringPass = () => {
+
+        console.log("Entering password...")
+    }
+    render() {
+        return (
+            <input type="password" onKeyUp={this.enteringPass}/>
+        )
+      }
+}
